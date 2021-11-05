e1e5adf84b5462141d30efaa0e9060c3f91c8396
Nam Tran <83447048+mentatss@users.noreply.github.com>
2021-11-05 16:02:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from SPARQLWrapper import SPARQLWrapper, JSON, CSV
 import io
 import time
 
+#Get 1st degree neighbour of origin article
+#When use with local endpoint, remove the line '?linkto  a  owl:Thing.' because local database does not have that
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setQuery("""
 SELECT ?id
@@ -16,12 +18,13 @@ WHERE {
 """)
 sparql.setReturnFormat(CSV)
 results = sparql.query().convert().decode('u8')
-
+#read csv returen from query, add the neighbor to the list
 c=pandas.read_csv(io.StringIO(results))
 neighbor=c.id.to_list()
 
-rtime=0
 
+#for each 1st degree neighbour, do the same query to find 2nd degree neighbour and add to the list
+#also remove the line '?linkto  a  owl:Thing.' when use on local endpoint
 for a in range(len(neighbor)):
     sparql.setQuery(f'''
         SELECT ?id
@@ -34,11 +37,8 @@ for a in range(len(neighbor)):
     ''')
     sparql.setReturnFormat(CSV)
     results = sparql.query().convert().decode('u8')
-    start=time.time()
     c=pandas.read_csv(io.StringIO(results))
     neighbor+=c.id.to_list()
-    run=time.time()-start
-    rtime+=run
 
+    
 print(len(neighbor))
-print(rtime)
