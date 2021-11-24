62cc560dc827e48f39dd5afc9f5d0d979da1b359
Nam Tran <83447048+mentatss@users.noreply.github.com>
2021-11-24 20:44:35 +0100

    Add files via upload
    update comments

--- workloadopt.py
+++ workloadopt.py
@@ -2,28 +2,34 @@ import pandas as pd
 import random
 
 def Opt_RF(query, prediction_model, bounded_time):
-    profit = 0
-    query_set = pd.read_csv(query)
-    model = pd.read_pickle(prediction_model)
+    query_set = pd.read_csv(query)              #set of queries to be executed
+    model = pd.read_pickle(prediction_model)    #prediction model
 
+    #predict the runtime of the received set of queries
     query_set['runtime_prediction'] = model.predict(query_set.filter(items=['in_src', 'out_src', 'in_dst', 'out_dst', 'bound'], axis=1))
+
+    #calculate profit to runtime ratio of each query
     for index, row in query_set.iterrows():
         query_set.at[index, 'ratio']  = row['profit']/row['runtime_prediction']
+
+    #sort ratio and update index
     query_set.sort_values(by=['ratio'], ascending=False, inplace=True)
     query_set = query_set.reset_index(drop=True)
 
-    actual_time = 0
+    #Knapsack solver (based on predicted runtime)
+    actual_time, profit = 0
     for index, row in query_set.iterrows():
         while actual_time < bounded_time:
             actual_time += row['runtime_prediction']
             profit += row['profit']
+            
     return(profit)
 
 def Opt_Rnd(query, bounded_time):
-    profit = 0
-    query_set = pd.read_csv(query)
+    query_set = pd.read_csv(query)  #set of queries to be executed
 
-    actual_time = 0
+    #Knapsack solver (random)
+    actual_time, profit = 0
     while actual_time < bounded_time:
         index = random.randint(0, len(query_set))
         profit += query_set.at[index, 'profit']
@@ -33,16 +39,18 @@ def Opt_Rnd(query, bounded_time):
 
 
 def Opt_True(query, bounded_time):
-    profit = 0
-    query_set = pd.read_csv(query)
+    query_set = pd.read_csv(query)  #set of queries to be executed
 
+    #calculate profit to runtime ratio of each query
     for index, row in query_set.iterrows():
         query_set.at[index, 'ratio']  = row['profit']/row['runtime']
 
+    #sort ratio and update index
     query_set.sort_values(by=['ratio'], ascending=False, inplace=True)
     query_set = query_set.reset_index(drop=True)
     
-    actual_time = 0
+    #Knapsack solver (based on real rumtime)
+    actual_time, profit = 0
     for index, row in query_set.iterrows():
         while actual_time < bounded_time:
             actual_time += row['runtime']
@@ -50,6 +58,3 @@ def Opt_True(query, bounded_time):
 
     return(profit)
 
-print(Opt_RF('data/workloadopt.csv', 'reachable_no_opt.pkl', 300000))
-
-# print(Opt_RF('data/workloadopt.csv', 300000))
